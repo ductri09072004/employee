4af305600570f515b1b06ce2659067e8cd1fa99f
BlueDuck <145476276+ductri09072004@users.noreply.github.com>
2025-06-11 09:53:45 +0700

    feat: 11/6

--- obj/Debug/net8.0/JOLICOW.AssemblyInfo.cs
+++ obj/Debug/net8.0/JOLICOW.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JOLICOW")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d368043b7498035017d35a6f1616877d9668177d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b164ace9bd166b36a972dc2fd929c42bbd96c2aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("JOLICOW")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JOLICOW")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
