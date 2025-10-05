365d2c9e0e028dc3c5d41fe74bf4d3682124c875
BlueDuck <145476276+ductri09072004@users.noreply.github.com>
2025-10-05 21:38:53 +0700

    fix: api login

--- obj/Debug/net8.0/JOLICOW.AssemblyInfo.cs
+++ obj/Debug/net8.0/JOLICOW.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JOLICOW")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2da2496ae4c51b0615bc53033a0137f1d4d4eee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a540ec867a65af71bb2f25b1a6f0a4132dd98b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("JOLICOW")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JOLICOW")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
